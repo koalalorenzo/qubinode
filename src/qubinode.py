2ba0b560353e1342e6e4388b2019bb34ee38cf8e
Lorenzo Setale <koalalorenzo@gmail.com>
2016-02-10 20:16:41 +0100

    Added references in the source code.

--- src/qubinode.py
+++ src/qubinode.py
@@ -53,12 +53,14 @@ NAMES = {
     'bc': 'Bitcoin Core',
     'xt': 'BitcoinXT',
     'bu': 'Bitcoin Unlimited',
+    'cl': 'Bitcoin Classic',
 }
 
 # Not sure what to put in here yet... I'm sure there will be something
 RELEASES = {
     'xt:0.11.0d': {},
     'bu:0.11.2': {},
+    'cl:0.11.2': {},
 }
 
 PROVIDERS = {
